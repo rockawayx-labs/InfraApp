b13efa4ce22d4a1c504a0e827cfa4f557f1db84c
turekmat <turekmatyas.tm@gmail.com>
2024-04-26 11:10:04 +0200

    ecosystems page

--- infraapp/src/app/api/ecosystems/route.js
+++ infraapp/src/app/api/ecosystems/route.js
@@ -1,5 +1,12 @@
 export async function GET(req) {
-  const ecosystems = await prisma.Ecosystem.findMany({});
+  const ecosystems = await prisma.Ecosystem.findMany({
+    orderBy: {
+      name: "asc",
+    },
+    include: {
+      groups: true,
+    },
+  });
   return new Response(JSON.stringify(ecosystems), {
     status: 200,
     headers: {
